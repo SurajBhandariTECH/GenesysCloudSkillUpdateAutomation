96ad31231cb0eef4211a44e401abde6554cdf586
SURAJ SINGH BHANDARI <83750485+SurajBhandariTECH@users.noreply.github.com>
2025-02-10 03:23:21 +0530

    Add files via upload

--- AgentExportService.java
+++ AgentExportService.java
@@ -1,11 +1,16 @@
 package com.surajDev.Bulk_Import_Feature.service;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mypurecloud.sdk.v2.ApiClient;
+import com.mypurecloud.sdk.v2.ApiException;
 import com.mypurecloud.sdk.v2.Configuration;
+import com.mypurecloud.sdk.v2.api.UsersApi;
+import com.mypurecloud.sdk.v2.model.UserEntityListing;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -14,7 +19,7 @@ public class AgentExportService {
 	@Autowired 
 	private HttpSession session;
 	
-	public void exportAgents(HttpServletResponse response) throws IOException {
+	public void exportAgents(HttpServletResponse response) throws IOException, ApiException {
 		//set response header for csv file
 		response.setContentType("text/csv");
 		response.setHeader("content-Disposition", "attachment; filename = agent_export.csv");
@@ -34,5 +39,29 @@ public class AgentExportService {
 		Configuration.setDefaultApiClient(apiClient);
 		
 		//fetch agent list from genesys cloud
+		int pageSize = 10; // Max allowed page size
+		int pageNumber = 1;
+		List<String> employerInfo = Arrays.asList("locations","skills","languages","authorization");
+		
+		 UsersApi usersApi = new UsersApi(apiClient);
+		 UserEntityListing agents = usersApi.getUsers(pageSize, pageNumber, null, null, null, employerInfo,
+					null, null);
+		 
+		 String[] header = { "Name", "Email", "Title", "Department",
+                  "Location Work", "Role Divisions", "Queues", "Division", "Skill" };
+		 
+		 //write each agent details
+		 for(var user:agents.getEntities()) {
+			 String[] row = {
+					 user.getName(),
+					 user.getEmail(),
+					 user.getTitle(),
+					 user.getDepartment(),
+					 user.getLocations().toString(),
+					 user.getAuthorization().roles()
+					 
+					 
+			 }
+		 }
 	}
 }
